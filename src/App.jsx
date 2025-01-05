77ec747cdc37597ad6c82fb5e61056226de1a18e
Abhishek K M <67158080+Sync271@users.noreply.github.com>
2025-01-05 15:02:06 +0530

    feat(Table): set up basic table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useLayoutEffect, useState } from "react";
+import TableComponent from "./components/Table";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  const [data, setData] = useState([]);
+
+  useLayoutEffect(() => {
+    (async () => {
+      const res = await fetch(
+        "https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json"
+      );
+
+      setData(await res.json());
+    })();
+  }, []);
+
+  return <TableComponent data={data} />;
 }
 
 export default App;
---
+++ src/components/Table/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function TableComponent({ data = [] }) {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>S.No.</th>
+            <th>Percentage funded</th>
+            <th>Amount pledged</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row) => (
+            <tr>
+              <td>{row?.["s.no"]}</td>
+              <td>{row?.["percentage.funded"]}</td>
+              <td>{row?.["amt.pledged"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
